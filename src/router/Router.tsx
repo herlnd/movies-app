224e4acf25c18883c9c8e8196e0828cccdb07e6f
Hernán Landoni <76965458+herlnd@users.noreply.github.com>
2022-09-07 23:52:46 -0300

    Update Router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,7 @@
-import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Movie from '../pages/Movie';
 import NotFound from '../pages/NotFound';
-
-useParams;
 
 const Router = () => {
 	return (
