ab9d8da71652601b3d4016597f3c87e82884fa15
herlnd <hernanlandoni@gmail.com>
2022-11-02 21:42:16 -0300

    fixed alignment on main screen

--- src/components/Navigation/MainNavigation.tsx
+++ src/components/Navigation/MainNavigation.tsx
@@ -1,20 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const MainNavigation = () => {
   return (
-    <header className="p-8 flex items-center justify-between">
-      <h1 className="btn btn-ghost text-slate-100 normal-case text-2xl">
-        <NavLink to="/">Movies App</NavLink>
+    <header className='p-8 flex items-center justify-between'>
+      <h1 className='btn btn-ghost text-slate-100 normal-case text-2xl'>
+        <NavLink to='/'>Movies App</NavLink>
       </h1>
-      <nav>
-        <ul className="flex gap-8">
-          <li>
-            <button className="btn btn-ghost text-slate-100 normal-case text-xl">
-              <NavLink to="/">Discover</NavLink>
-            </button>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
--- src/components/Search/SearchSection.tsx
+++ src/components/Search/SearchSection.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import SearchForm from "./SearchForm";
+import { useState } from 'react';
+import SearchForm from './SearchForm';
 
 interface searchSectionType {
   scrollToSection: () => void;
@@ -7,7 +7,7 @@ interface searchSectionType {
 }
 
 const SearchSection = (props: searchSectionType) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
   const goToDiscoverHandler = () => {
     props.scrollToSection();
@@ -20,23 +20,23 @@ const SearchSection = (props: searchSectionType) => {
   props.searchTerm(searchTerm);
 
   return (
-    <section className="hero-content text-center text-slate-100">
-      <div className="max-w-xl">
-        <h1 className="mb-5 text-5xl font-bold">
+    <section className='flex justify-center items-center text-center text-slate-100'>
+      <div className='max-w-xl'>
+        <h1 className='mb-5 text-5xl font-bold'>
           Discover the latest blockbusters
         </h1>
-        <p className="text-slate-200 text-lg pb-6">
+        <p className='text-slate-200 text-lg pb-6'>
           See what movies are trending right now on theatres and on-demand
           platforms.
         </p>
         <button
-          className="btn w-32 bg-slate-100 border-slate-100 text-gray-800 hover:bg-slate-300 hover:border-slate-300"
+          className='btn w-32 bg-slate-100 border-slate-100 text-gray-800 hover:bg-slate-300 hover:border-slate-300'
           onClick={goToDiscoverHandler}
         >
           Discover
         </button>
-        <div className="divider py-4 font-medium">OR</div>
-        <p className="text-slate-100 text-lg pb-6">Take your pick!</p>
+        <div className='divider py-4 font-medium'>OR</div>
+        <p className='text-slate-100 text-lg pb-6'>Take your pick!</p>
         <SearchForm searchTerm={showSearchTerm} />
       </div>
     </section>
